28c8db06ac09ed37a538bdf90f2eb16ff2d3489b
Kevin de Bree <kpdebree@gmail.com>
2017-09-28 02:14:31 -0700

    Update 4

--- assets/app.js
+++ assets/app.js
@@ -3,9 +3,9 @@ $(document).ready(function(){
 	var giphies = [
 	"doge", "kitten", "cat", "dog", "pusheen", "daschund", "puppy", "hotline bling", "panda"]
 
-	var api_key = "V4z9F4UVJ7hDZGf4jvuf4lpbmazhfcEH"
+	var api_key = "&api_key=dc6zaTOxFJmzC&limit=10";
 
-	var base_url = "http://api.giphy.com/v1/gifs/search?q="
+	var base_url = "https://api.giphy.com/v1/gifs/search?q=";
 
 	var queryURL;
 	var category;
@@ -55,32 +55,36 @@ $(document).ready(function(){
 
 
 		$.ajax({
-			url: queryURL
+			url: queryURL,
 			method: "GET"
 		})
 		.done(function(response) {
 			var results = response.data;
+			console.log(results)
 
 			for (i = 0; i < results.length; i++) {
-				var giphyDiv = $("<div class\"giphy-item\">");
+				var giphyDiv = $("<div class='item'>");
+
+				console.log(giphyDiv)
+				var rating = results[i].rating;
+				var p = $("<p>").text("Rating: " + rating);
+				var giphy_image = $("<img>");
+
+				var animated_state = results[i].images.fixed_height.url;
+				var still_state = results[i].images.fixed_height_still.url;
+
+				giphy_image.attr("src", still_state);
+				giphy_image.attr("data-still", still_state);
+				giphy_image.attr("data-animate", animated_state);
+				giphy_image.attr("data-state", "still_state");
+
+				giphyDiv.append(p);
+				giphyDiv.append(giphy_image);
+
+				$("#giphy-container").append(giphyDiv);
 			}
-
-
-
-
-		})
-
-
-
-
-
-
-
-
-	})
-
-
-
+		});
+	});
 
 
 	generateButtons(giphies, "giphy-button", "#button-container");
