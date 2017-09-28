1ea5ad90bce6f50774e2d64bba394549565c5347
kpdebree <kpdebree@gmail.com>
2017-09-28 13:18:27 -0700

    Update 5

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
 	var giphies = [
-	"doge", "kitten", "cat", "dog", "pusheen", "daschund", "puppy", "hotline bling", "panda"]
+	"doge", "kitten", "cat", "dog", "pusheen", "daschund", "puppy", "hotline bling", "panda", "eminem", "rick and morty", "get schwifty", "mr. poopy butthole", "south park"]
 
 	var api_key = "&api_key=dc6zaTOxFJmzC&limit=10";
 
@@ -77,6 +77,7 @@ $(document).ready(function(){
 				giphy_image.attr("data-still", still_state);
 				giphy_image.attr("data-animate", animated_state);
 				giphy_image.attr("data-state", "still_state");
+				giphy_image.addClass("giphy-image")
 
 				giphyDiv.append(p);
 				giphyDiv.append(giphy_image);
@@ -85,6 +86,45 @@ $(document).ready(function(){
 			}
 		});
 	});
+
+
+	//Animate Giphies
+	$(document).on("click", ".giphy-image", function(){
+
+		var state = $(this).attr("data-state");
+
+
+		if(state === "still_state") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animated_state");
+      } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still_state");
+      }
+
+	});
+
+$("#submit-button").on("click", function(event) {
+
+
+    event.preventDefault();
+
+
+    var newGiphy = $("input").val().trim();
+
+    if (newGiphy.length > 2) {
+
+
+      giphies.push(newGiphy);
+
+
+    }
+
+    generateButtons(giphies, "giphy-button", "#button-container");
+
+  });
+
+
 
 
 	generateButtons(giphies, "giphy-button", "#button-container");
